aac21d51c3ffbf942c6f9ae5585b21e42d62d168
Amio <amio.cn@gmail.com>
2017-08-03 13:37:33 +0800

    Use stream for minimal memory footprint.

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const words = require('words-en')
+const fs = require('fs')
+const readline = require('readline')
+const wordsTxtPath = require('words-en/path')
 
-module.exports = function (wildcard) {
+module.exports = async function (wildcard) {
   const regString = wildcard.replace(/\?/g, '\\w').replace(/\*/, '\\w*')
-  const pattern = new RegExp('\\b' + regString + '\\b', 'g')
+  const pattern = new RegExp('^' + regString + '$')
 
-  return words.match(pattern)
+  return await new Promise((resolve, reject) => {
+    const results = []
+
+    const rl = readline.createInterface({
+      input: fs.createReadStream(wordsTxtPath)
+    })
+
+    rl.on('line', line => {
+      pattern.test(line) && results.push(line)
+    })
+
+    rl.on('close', () => resolve(results))
+    rl.on('error', e => reject(e))
+  })
 }
